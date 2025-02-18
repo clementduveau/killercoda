4174408cb44789ee84c43a6d4569166a4dd0b0ae
Clément Duveau <clement@duveau.eu>
2025-02-18 17:23:01 +0100

    Keep history and don't delete submodule

--- opentelemetry-instrumentation-demo/background.sh
+++ opentelemetry-instrumentation-demo/background.sh
@@ -3,5 +3,5 @@
 # Install JRE
 apt update; apt install -y openjdk-17-jdk openjdk-17-jre;
 
-# Pull k6 image
+# Pull k6 image for later
 docker pull grafana/k6:latest;
--- opentelemetry-instrumentation-demo/foreground.sh
+++ opentelemetry-instrumentation-demo/foreground.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
-echo "Building instance..."
+echo "Building instance...";
+
+# Clean up the environment
+# mv /root/course/opentelemetry-workshop/source/rolldice /root/course/rolldice; rm -r opentelemetry-workshop;
 
 # Move to working directory and start services
-cd /root/course
-docker-compose -f docker-compose.yaml up -d
-mv opentelemetry-workshop/source/rolldice rolldice; rm -r opentelemetry-workshop
+cd /root/course;
+docker-compose -f docker-compose.yaml up -d;
 
-clear; docker ps; echo ">> Environment ready!"
+docker ps; echo ">> Environment ready!";
