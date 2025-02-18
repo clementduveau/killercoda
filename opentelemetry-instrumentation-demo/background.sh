d925c9162a3f2e3a4610e1771f26233f28255832
Clément Duveau <clement@duveau.eu>
2025-02-18 17:30:10 +0100

    Remove submodule, pre-maven

--- opentelemetry-instrumentation-demo/background.sh
+++ opentelemetry-instrumentation-demo/background.sh
@@ -5,3 +5,12 @@ apt update; apt install -y openjdk-17-jdk openjdk-17-jre;
 
 # Pull k6 image for later
 docker pull grafana/k6:latest;
+
+# Prepare Java
+cd /root/course/rolldice;
+./mvnw clean package -DskipTests;
+version=v2.6.0-beta.2;
+jar=opentelemetry-javaagent.jar;
+if [[ ! -f ./${jar} ]] ; then
+    curl -sL https://github.com/grafana/grafana-opentelemetry-java/releases/download/${version}/grafana-opentelemetry-java.jar -o ${jar};
+fi
--- opentelemetry-instrumentation-demo/foreground.sh
+++ opentelemetry-instrumentation-demo/foreground.sh
@@ -1,12 +1,9 @@
 #!/usr/bin/env bash
 
-echo "Building instance...";
-
-# Clean up the environment
-# mv /root/course/opentelemetry-workshop/source/rolldice /root/course/rolldice; rm -r opentelemetry-workshop;
+printf "Building instance..."
 
 # Move to working directory and start services
 cd /root/course;
 docker-compose -f docker-compose.yaml up -d;
 
-docker ps; echo ">> Environment ready!";
+docker ps; printf "\n\n\n>> Environment ready!\n\n\n";
