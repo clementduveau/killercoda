d18411df552015b551f0436a87932ae2a8060621
Clément Duveau <clement@duveau.eu>
2025-02-18 18:30:30 +0100

    fix scripts

--- opentelemetry-instrumentation-demo/background.sh
+++ opentelemetry-instrumentation-demo/background.sh
@@ -1,16 +1,11 @@
-#!/usr/bin/env bash
+apt update
+apt install -y openjdk-17-jdk openjdk-17-jre
 
-# Install JRE
-apt update; apt install -y openjdk-17-jdk openjdk-17-jre;
+docker pull grafana/k6:latest
 
-# Pull k6 image for later
-docker pull grafana/k6:latest;
+cd /root/course/rolldice
+./mvnw clean package -DskipTests
 
-# Prepare Java
-cd /root/course/rolldice;
-./mvnw clean package -DskipTests;
-version=v2.6.0-beta.2;
-jar=opentelemetry-javaagent.jar;
-if [[ ! -f ./${jar} ]] ; then
-    curl -sL https://github.com/grafana/grafana-opentelemetry-java/releases/download/${version}/grafana-opentelemetry-java.jar -o ${jar};
-fi
+version=v2.6.0-beta.2
+jar=opentelemetry-javaagent.jar
+curl -sL https://github.com/grafana/grafana-opentelemetry-java/releases/download/${version}/grafana-opentelemetry-java.jar -o ${jar}
--- opentelemetry-instrumentation-demo/foreground.sh
+++ opentelemetry-instrumentation-demo/foreground.sh
@@ -1,8 +1,7 @@
-#!/usr/bin/env bash
+printf "Building instance..."
 
-printf "Building instance...";
-cd /root/course/lgtm;
-docker-compose -f docker-compose.yaml up -d;
-clear;
-docker ps;
-printf "\n\n\n>> Environment ready!\n\n\n";
+cd /root/course/lgtm
+docker-compose -f docker-compose.yaml up -d
+
+docker ps
+printf "\n\n\n>> Environment ready!\n\n\n"
